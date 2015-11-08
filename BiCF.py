d167416132adf2e8cda9528cc7b59d00b412855a
Xiaoxu <xiaoxul@andrew.cmu.edu>
2015-11-07 22:15:48 -0500

    fix

--- BiCF.py
+++ BiCF.py
@@ -1,7 +1,65 @@
 __author__ = 'Ariel'
+import pickle
+import numpy as np
+import memory
+import model
 
-def memory():
 
-    
-    return
+def bi_item(m, tuples, k, func, func_w):
+    # with open('u2U.txt', 'r') as f1:
+    #     u2U = pickle.loads(f1.read())
+    # with open('m2Ucluster.txt', 'r') as f4:
+    #     m2Ucluster = pickle.loads(f4.read())
+    with open('m2M.txt', 'r') as f1:
+        m2M = pickle.loads(f1.read())
+    with open('u2Mcluster.txt', 'r') as f2:
+        u2Mcluster = pickle.loads(f2.read())
 
+    res = [] # return the list of predictions given the query tuples
+    # m = np.asarray(m, order = 'F') # column-major order
+    u2Mcluster = np.asarray(u2Mcluster, order = 'F')
+    simPair = model.similarityPair(u2Mcluster , func)
+    for pair in tuples:
+        #print pair[0]
+        c = findCentroid(m2M, pair[0])
+        sim = simPair[c]
+        temp = model.knn(sim, k)
+        prediction = model.predict(u2Mcluster, temp, sim, func_w)
+        pred = prediction[pair[1]] + 3
+        if pred > 5:
+            pred = 5
+        elif pred < 1:
+            pred = 1
+        res.append(pred)# plus 3
+        #print pred
+    return res
+
+def bi_user(m, tuples, k, func, func_w):
+    with open('u2U.txt', 'r') as f1:
+        u2U = pickle.loads(f1.read())
+    with open('m2Ucluster.txt', 'r') as f2:
+        m2Ucluster = pickle.loads(f2.read())
+    m2Ucluster = m2Ucluster.transpose()
+
+    res = [] # return the list of predictions given the query tuples
+    m2Ucluster = np.asarray(m2Ucluster, order = 'F')
+    simPair = model.similarityPair(m2Ucluster , func)
+    for pair in tuples:
+        #print pair[0]
+        c = findCentroid(u2U, pair[1])
+        sim = simPair[c]
+        temp = model.knn(sim, k)
+        prediction = model.predict(m2Ucluster, temp, sim, func_w)
+        pred = prediction[pair[0]] + 3
+        if pred > 5:
+            pred = 5
+        elif pred < 1:
+            pred = 1
+        res.append(pred)# plus 3
+        #print pred
+    return res
+
+def findCentroid(dict, target):
+    for key,values in dict.iteritems():
+        if  target in values:
+            return key
--- main.py
+++ main.py
@@ -7,45 +7,63 @@ import writeHelper
 import model
 import memory
 import BiCluster
+import BiCF
 import pickle
 
 def main():
     start_time = time.time()
     parser = argparse.ArgumentParser(description = "collaborative filtering")
+    parser.add_argument("-t", help = "if running test", action = 'store_true', default = False)
     parser.add_argument("-m", help = "memory-based or model collaborative filtering", choices = ['memory','model'])
     parser.add_argument("-k", help = "number of k nearest neighborhood", type = int, choices = [10,100,500])
     parser.add_argument("-s", help = "similarity metric used for knn ", choices = ['dotp','cosine'])
     parser.add_argument("-w", help = "approach for combining prediction given knn", choices = ['mean','weight'])
     parser.add_argument("-p", help = "if standardization used", action = 'store_true', default = False)
-
+    parser.add_argument("-b", help = "if bipartite clustering userd", action = "store_true", default = False)
     args = parser.parse_args()
     print args
-    output = "-".join([args.m, str(args.k), args.s, args.w])
-    if args.p:
-        output += '-pcc'
-    output += '.txt'
-    train = 'train.csv'
-    dev = 'dev.csv'
-    if args.m == 'memory':
-        M = readHelper.readTrainMemory(train)
-        query, tuples = readHelper.readQueryMemory(dev)
-        if args.p:
-            pred = memory.pccMemoryCF(M, query, args.k, args.s, args.w)
-        else:
-            pred = memory.memoryCF(M, query, args.k, args.s, args.w)
 
-        writeHelper.writePredMemory(output, pred, tuples)
+    if not args.t:
+        output = "-".join([args.m, str(args.k), args.s, args.w])
+        if args.p:
+            output += '-pcc'
+        if args.b:
+            output += '-bi'
+        output += '.txt'
+        train = 'train.csv'
+        dev = 'dev.csv'
+
+        if args.m == 'memory' and not args.b:
+            M = readHelper.readTrainMemory(train)
+            query, tuples = readHelper.readQueryMemory(dev)
+            if args.p:
+                pred = memory.pccMemoryCF(M, query, args.k, args.s, args.w)
+            else:
+                pred = memory.memoryCF(M, query, args.k, args.s, args.w)
+
+            writeHelper.writePredMemory(output, pred, tuples)
+        elif args.m == 'model' and not args.b:
+            M = readHelper.readTrainModel(train)
+            tuples = readHelper.readQueryModel(dev)
+            if args.p:
+                pred = model.pccModelCF(M, tuples, args.k, args.s, args.w)
+            else:
+                pred = model.modelCF(M, tuples, args.k, args.s, args.w)
+            writeHelper.writePredModel(output, pred)
+
+        if args.b:
+            M = readHelper.readTrainModel(train)
+            tuples = readHelper.readQueryModel(dev)
+            if args.m == 'model':
+                pred = BiCF.bi_item(M, tuples, args.k, args.s, args.w)
+            else:
+                pred = BiCF.bi_user(M, tuples, args.k, args.s, args.w)
+            writeHelper.writePredModel(output, pred)
+
+        print 'time: %s seconds' % (time.time()-start_time)
     else:
-        M = readHelper.readTrainModel(train)
-        tuples = readHelper.readQueryModel(dev)
-        if args.p:
-            pred = model.pccModelCF(M, tuples, args.k, args.s, args.w)
-        else:
-            pred = model.modelCF(M, tuples, args.k, args.s, args.w)
+        runTest()
 
-        writeHelper.writePredModel(output, pred)
-
-    print 'time: %s seconds' % (time.time()-start_time)
 
 def getBiCluster(k1, k2):
     train = 'train.csv'
@@ -57,7 +75,7 @@ def getBiCluster(k1, k2):
     # write to file
     with open('u2U.txt', 'w') as f1:
         pickle.dump(u2U, f1)
-    with open('w2W.txt', 'w') as f2:
+    with open('m2M.txt', 'w') as f2:
         pickle.dump(m2M, f2)
     with open('u2Mcluster.txt', 'w') as f3:
         pickle.dump(u2Mcluster, f3)
@@ -66,6 +84,21 @@ def getBiCluster(k1, k2):
     print 'time: %s seconds' % (time.time()-start_time)
     print ClusterTime
     return ClusterTime
+
+def runTest():
+    train = 'train.csv'
+    test = 'test.csv'
+    output = 'prediciton.txt'
+    M = readHelper.readTrainMemory(train)
+    query, tuples = readHelper.readQueryMemory(test)
+    k = 100
+    func = 'dotp'
+    func_w = 'mean'
+    pred = memory.pccMemoryCF(M, query, k, func, func_w)
+    writeHelper.writePredMemory(output, pred, tuples)
+    return
+
 if __name__ == '__main__':
-    #main()
-    getBiCluster(300,100)
+    main()
+    #getBiCluster(3000,1500)
+
